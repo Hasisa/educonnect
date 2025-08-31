abac9b7fd1241f67c686d5b2c4aeb840a011cdc9
hesen ismayilov <hesenismayilov@hesens-MacBook-Air.local>
2025-08-31 21:01:40 +0400

    schaize

--- registration-and-login/dashboard/aichat/server.js
+++ registration-and-login/dashboard/aichat/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", FRONTEND_URL);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
+  if (req.method === "OPTIONS") return res.sendStatus(200); // preflight
   next();
 });
 
